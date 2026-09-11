69934c5dbbdef15835800f07e861cabb1992ec5a
Veritas <jadeliwilson09@gmail.com>
2026-09-11 11:54:20 +1000

    Update post.py
    i have made The Broad Strokes (they are nonsensical but they are.. something)

--- post.py
+++ post.py
@@ -8,24 +8,47 @@ class Post:
     You will need to track up votes more cleverly than previously because
     a user is only allowed to vote *once*.
     """
+
+    self = 
+    content = 
+    author =
+
+    new post = (self, content, author etc)
+
+    votes = sum user per vote #retrieve int vote from server?
+
+    
     pass
   
   def get_author(self):
     """
     Returns the author of the Post.
     """
+
+    author = arr | string / author
+
+    
     pass
   
   def get_content(self):
     """
     Returns the content of the Post.
     """
+
+    get content = post | pos / content = content
+    
     pass
   
   def get_upvotes(self):
     """
     Returns a non-negative integer representing the total number of upvotes.
     """
+
+  ^
+  upvotes = upvotes 
+  return
+
+    
     pass
   
   def set_content(self, content, by_user):
@@ -33,6 +56,16 @@ class Post:
     Called when the given user wants to update the content.
     * raises PermissionDenied if the given user is not the author.
     """
+
+    if user = user then
+    this.content = that.content
+
+    else
+    
+    return error
+    drop content
+    
+    
     pass
   
   def upvote(self, by_user):
@@ -40,4 +73,11 @@ class Post:
     Called when the given user wants to upvote this post.
     A user can only perform an up vote *once*.
     """
+
+    self =
+
+    by_user = when the upvote
+    tally on to string or something idk
+
+    
     pass
